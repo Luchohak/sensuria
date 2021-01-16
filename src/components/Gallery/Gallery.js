6e7d07b26c1b597b91543342d34a1be311327fad
Luchohak <a00958934@gmail.com>
2021-01-16 07:30:46 -0700

    Nginx and Docker configuration to use in production and beginning of gallery section

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './Gallery.css';
+import { Container, Row, Col, Image } from 'react-bootstrap'
+
+import imageExample from './../../assets/carousel/far-slider.jpeg';
 
 const gallery = () => {
-    return(
-        <h1>Gallery Page
-            <h1>Gallery Page</h1>
-        </h1>
+    return (
+        <Container>
+            <Row>
+                <Col sm={6} md={4} lg={3}>
+                    <Image src={imageExample} thumbnail />
+                </Col>
+                <Col sm={6} md={4} lg={3}>
+                    <Image src={imageExample} thumbnail />
+                </Col>
+                <Col sm={6} md={4} lg={3}>
+                    <Image src={imageExample} thumbnail />
+                </Col>
+                <Col sm={6} md={4} lg={3}>
+                    <Image src={imageExample} thumbnail />
+                </Col>
+            </Row>
+            
+        </Container>
     );
 };
 
