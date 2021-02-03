8447735f3d9a80009b84ddc271600b0e8a464760
Luchohak <a00958934@gmail.com>
2021-02-03 10:50:15 -0700

    Add links to music items buttons and embedded Spotify player

--- src/components/Music/MusicItems/MusicItem/MusicItem.js
+++ src/components/Music/MusicItems/MusicItem/MusicItem.js
@@ -3,9 +3,22 @@ import { Row, Col, Image } from 'react-bootstrap';
 
 import './MusicItem.css';
 import spotiIcon from '../../../../assets/icons/spotify-100.png';
+import appMusicIcon from '../../../../assets/icons/itunes-100.png';
+import amzMusicIcon from '../../../../assets/icons/amazon-music-128.png';
 
 
 const MusicItem = (props) => {
+
+    let embededSong = <p>Comming soon!</p>
+
+    if (props.isReleased) {
+        embededSong = <iframe 
+            src={props.embededURL}
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media">
+        </iframe>
+    }
     return (
         <Col md={5} className="mx-4 my-4 music-item bg1 py-4 px-5">
             <Row><Image src={props.cover} /></Row>
@@ -13,18 +26,19 @@ const MusicItem = (props) => {
                 <Col md={5}>
                     {props.title}
                 </Col>
-                <Col md={7} className="d-flex align-items-center listen-buttons">
+                <Col md={7} className={props.isReleased ? "d-flex align-items-center listen-buttons" : "d-none"}>
                     <div className="bg-classic">
-                        <a href={props.spotifyURL} target="_blank"><Image src={spotiIcon}></Image></a>
+                        <a href={props.spotifyURL} target="_blank"><Image src={spotiIcon} disable></Image></a>
                     </div>
                     <div className="bg-classic mx-3">
-                        <a href={props.appMusicURL} target="_blank"><Image src={spotiIcon}></Image></a>
+                        <a href={props.appMusicURL} target="_blank"><Image src={appMusicIcon}></Image></a>
                     </div>
                     <div className="bg-classic">
-                        <a href={props.amzMusicURL} target="_blank"><Image src={spotiIcon}></Image></a>
+                        <a href={props.amzMusicURL} target="_blank"><Image src={amzMusicIcon}></Image></a>
                     </div>
                 </Col>
             </Row>
+            <Row>{embededSong}</Row>
         </Col>
     );
 }
--- src/components/Music/MusicItems/MusicItems.js
+++ src/components/Music/MusicItems/MusicItems.js
@@ -10,42 +10,64 @@ import cynicalCover from '../../../assets/album-covers/cynical-cover.png'
 import './MusicItems.css';
 
 const MusicItems = () => {
+
+//Spotify Links 
+const spotRiq = 'https://open.spotify.com/track/2ycdnfqQlths50dEitGCZ7?si=U3wY6t3GTM2DuPwLbYaEgQ';
+const spotFar = 'https://open.spotify.com/track/66emZup8KqjJZTWPITFrFO?si=EEl66q0XRO-bI8E3c5hHQA';
+const spotMonsters = '';
+const spotCynical = '';
+//Embeded Links
+const riqEmbeded = 'https://open.spotify.com/embed/track/2ycdnfqQlths50dEitGCZ7'
+const farEmbeded = 'https://open.spotify.com/embed/track/66emZup8KqjJZTWPITFrFO';
+const monstersEmbeded = '';
+const cynicalEmbeded = '';
+//Itunes Links
+const iRiq = 'https://music.apple.com/mx/album/riqu%C3%ADsimo/1547015670?i=1547015671';
+const iFar = 'https://music.apple.com/mx/album/far/1547587437?i=1547587438';
+const iMonsters = '';
+const iCynical = '';
+//Amazon Music Links 
+const amzRiq = 'https://music.amazon.com.mx/albums/B08RSLFKVB?trackAsin=B08RSLV2JX';
+const amzFar = 'https://music.amazon.com.mx/albums/B08S6PTDP4?trackAsin=B08S6KHKPD';
+const amzMonsters = '';
+const amzCynical = '';
+
     const musicItemsInfo = [
         {
             songTitle: 'Riquísimo',
             coverImg: riqCover,
-            spotify: '',
-            appMusic: '',
-            amzMusic: '',
-            audio: '',
-            coverClass: ''
+            spotifyEmbededURL: riqEmbeded,  
+            spotify: spotRiq,
+            appMusic: iRiq,
+            amzMusic: amzRiq,
+            released: true,
         },
         {
             songTitle: 'FAR',
+            spotifyEmbededURL: farEmbeded,
             coverImg: farCover,
-            spotify: '',
-            appMusic: '',
-            amzMusic: '',
-            audio: '',
-            coverClass: ''
+            spotify: spotFar,
+            appMusic: iFar,
+            amzMusic: amzFar,
+            released: true,
         },
         {
             songTitle: 'Monsters',
             coverImg: monstersCover,
-            spotify: '',
-            appMusic: '',
-            amzMusic: '',
-            audio: '',
-            coverClass: ''
+            spotifyEmbededURL: monstersEmbeded,
+            spotify: spotMonsters,
+            appMusic: iMonsters,
+            amzMusic: amzMonsters,
+            released: false,
         },
         {
             songTitle: 'Cynical',
             coverImg: cynicalCover,
-            spotify: '',
-            appMusic: '',
-            amzMusic: '',
-            audio: '',
-            coverClass: ''
+            spotifyEmbededURL: cynicalEmbeded,
+            spotify: spotCynical,
+            appMusic: iCynical,
+            amzMusic: amzCynical,
+            released: false,
         }
     ]
     return (
@@ -56,11 +78,13 @@ const MusicItems = () => {
                         <MusicItem
                             title={item.songTitle}
                             cover={item.coverImg}
+                            embededURL={item.spotifyEmbededURL}
                             spotifyURL={item.spotify}
                             appMusicURL={item.appMusic}
                             amzMusicURL={item.amzMusicURL}
                             song={item.audio}
                             imgClass={item.coverClass}
+                            isReleased={item.released}
                         />
                     );
                 })
